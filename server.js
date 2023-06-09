290c7b1adb2f7930ba0e2bb68e8a85968164a304
Daniel Houri <danielsaadia.houri@live.biu.ac.il>
2023-06-09 10:32:51 +0300

    Clean WaitList schdule

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require('body-parser');
+const WaitList = require('./models/WaitList');
+const schedule = require('node-schedule');
 
 const app = express();
 app.use(cors());
@@ -29,4 +31,18 @@ mongoose
 // Start the server and listen on a specific port
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
+});
+
+// Schedule the function to run every day at a specific time (e.g., 12:00 AM)
+const job = schedule.scheduleJob('0 0 * * *', async () => {
+    try {
+        const currentDate = new Date();
+
+        // Find and delete the wait lists whose date has passed
+        await WaitList.deleteMany({ date: { $lt: currentDate } });
+
+        console.log('Deleted expired wait lists.');
+    } catch (error) {
+        console.error('Error deleting wait lists:', error);
+    }
 });
